c4a73fe666afd573ccf762cedab9f925dea5294b
Pawel Radzinski <Pawel.Radzinski@gmail.com>
2019-03-19 08:41:12 +0100

    OK, now the files are generated. No overwrites. Some crude checks.

--- register-vcsh-in-mr.sh
+++ register-vcsh-in-mr.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
-template='[$HOME/.config/vcsh/repo.d/$NAME.git]\ncheckout = vcsh clone ${URL} ${NAME}
-'
+template='[$HOME/.config/vcsh/repo.d/$NAME.git]\ncheckout = vcsh clone ${URL} ${NAME}\n'
+
+# todo: check envsubst availability
+target_uber_dir=$HOME/.config/mr/available.d
+
+if [[ ! -d ${target_uber_dir} ]]; then
+	 echo "The direcory ${target_uber_dir} does not exist; about to create it..."
+fi	 
+
 cd
 for r in $(vcsh list); do 
 	export NAME="$r"
+	target_file=${target_uber_dir}/${NAME}.vcsh
+	if [[ -e ${target_file} ]]; then
+		echo -e "The file $target_file already exists, skipping...\n"
+		continue
+	fi	
 	export URL=$(vcsh "$r" remote -v | grep fetch | cut  -f2  | cut -d ' ' -f1)
-	echo -e ${template} | envsubst '$NAME $URL'
+	echo -e "Creating file $target_file\n"
+	echo -e ${template} | envsubst '$NAME $URL' > ${target_file}
 done
